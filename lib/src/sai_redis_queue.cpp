ac326a1edad310e80e5e0267e8ff3834a44f503d
Stephen Sun <stephens@nvidia.com>
2021-11-21 14:16:55 +0000

    Bulk API for queue - poc
    Signed-off-by: Stephen Sun <stephens@nvidia.com>
    
    Conflicts:
    	SAI

--- lib/src/sai_redis_queue.cpp
+++ lib/src/sai_redis_queue.cpp
@@ -3,8 +3,18 @@
 REDIS_GENERIC_QUAD(QUEUE,queue);
 REDIS_GENERIC_STATS(QUEUE,queue);
 
+REDIS_BULK_CREATE(QUEUE,queue);
+REDIS_BULK_REMOVE(QUEUE,queue);
+REDIS_BULK_SET(QUEUE,queue);
+REDIS_BULK_GET(QUEUE,queue);
+
 const sai_queue_api_t redis_queue_api = {
 
     REDIS_GENERIC_QUAD_API(queue)
     REDIS_GENERIC_STATS_API(queue)
+
+    redis_bulk_create_queue,
+    redis_bulk_remove_queue,
+    redis_bulk_set_queue,
+    redis_bulk_get_queue
 };
