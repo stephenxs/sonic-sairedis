1d0fb9f64a6601a2216eb46a5176f158c941ba3e
Stephen Sun <stephens@nvidia.com>
2021-11-21 14:16:55 +0000

    Bulk API for queue, scheduler group, ingress priority group - poc
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- vslib/sai_vs_buffer.cpp
+++ vslib/sai_vs_buffer.cpp
@@ -6,6 +6,11 @@ VS_GENERIC_QUAD(BUFFER_PROFILE,buffer_profile);
 VS_GENERIC_STATS(BUFFER_POOL,buffer_pool);
 VS_GENERIC_STATS(INGRESS_PRIORITY_GROUP,ingress_priority_group);
 
+VS_BULK_CREATE(INGRESS_PRIORITY_GROUP,ingress_priority_group);
+VS_BULK_REMOVE(INGRESS_PRIORITY_GROUP,ingress_priority_group);
+VS_BULK_SET(INGRESS_PRIORITY_GROUP,ingress_priority_group);
+VS_BULK_GET(INGRESS_PRIORITY_GROUP,ingress_priority_group);
+
 const sai_buffer_api_t vs_buffer_api = {
 
     VS_GENERIC_QUAD_API(buffer_pool)
@@ -13,4 +18,9 @@ const sai_buffer_api_t vs_buffer_api = {
     VS_GENERIC_QUAD_API(ingress_priority_group)
     VS_GENERIC_STATS_API(ingress_priority_group)
     VS_GENERIC_QUAD_API(buffer_profile)
+
+    vs_bulk_create_ingress_priority_group,
+    vs_bulk_remove_ingress_priority_group,
+    vs_bulk_set_ingress_priority_group,
+    vs_bulk_get_ingress_priority_group
 };
