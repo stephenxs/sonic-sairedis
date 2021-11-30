e3af42e58e74d87df9fc1ade693dc106cfa65608
Stephen Sun <stephens@nvidia.com>
2021-11-30 07:33:31 +0000

    Support bulk for port and scheduler group
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- vslib/src/sai_vs_port.cpp
+++ vslib/src/sai_vs_port.cpp
@@ -15,6 +15,11 @@ VS_GENERIC_QUAD(PORT_SERDES,port_serdes);
 VS_GENERIC_STATS(PORT,port);
 VS_GENERIC_STATS(PORT_POOL,port_pool);
 
+VS_BULK_CREATE(PORT,port);
+VS_BULK_REMOVE(PORT,port);
+VS_BULK_SET(PORT,port);
+VS_BULK_GET(PORT,port);
+
 const sai_port_api_t vs_port_api = {
 
     VS_GENERIC_QUAD_API(port)
@@ -28,4 +33,9 @@ const sai_port_api_t vs_port_api = {
     VS_GENERIC_QUAD_API(port_connector)
 
     VS_GENERIC_QUAD_API(port_serdes)
+
+    vs_bulk_create_port,
+    vs_bulk_remove_port,
+    vs_bulk_set_port,
+    vs_bulk_get_port
 };
