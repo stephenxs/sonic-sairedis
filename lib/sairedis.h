37995770d8e9ac773848b81c33164e5b3a0db0d1
Stephen Sun <stephens@nvidia.com>
2024-09-05 10:54:49 +0000

    Support setting bulk size
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- lib/sairedis.h
+++ lib/sairedis.h
@@ -140,6 +140,13 @@ typedef struct _sai_redis_flex_counter_group_parameter_t
      */
     sai_s8_list_t plugins;
 
+    /**
+     * @brief The bulk chunk size of the counter group
+     *
+     * It should be a number representing the bulk chunk size.
+     */
+    sai_s8_list_t bulk_chunk_size;
+
 } sai_redis_flex_counter_group_parameter_t;
 
 typedef struct _sai_redis_flex_counter_parameter_t
