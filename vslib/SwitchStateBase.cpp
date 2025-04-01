80da97d62d73729f1a03fb59ded986501aa6f6ee
Ze Gan <ganze718@gmail.com>
2025-04-01 16:01:31 +1100

    Merge pull request #36 from Pterosaur/update_sai
    Update SAI to v1.15.7

--- vslib/SwitchStateBase.cpp
+++ vslib/SwitchStateBase.cpp
@@ -4057,3 +4057,15 @@ sai_status_t SwitchStateBase::queryStatsCapability(
 
     return SAI_STATUS_NOT_SUPPORTED;
 }
+
+sai_status_t SwitchStateBase::queryStatsStCapability(
+    _In_ sai_object_id_t switchId,
+    _In_ sai_object_type_t objectType,
+    _Inout_ sai_stat_st_capability_list_t *stats_capability)
+{
+    SWSS_LOG_ENTER();
+
+    // TODO: Fix me
+
+    return SAI_STATUS_NOT_IMPLEMENTED;
+}
